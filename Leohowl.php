93d8813ef6f5f2c935d343c058caa752682ad7e7
leohowl <leo_howl@163.com>
2019-05-13 15:33:18 +0800

    edit notes

--- Leohowl.php
+++ Leohowl.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: HANSN-CZ-P01
  * Date: 2019-04-24
  * Time: 9:29 AM
  */
@@ -25,16 +23,13 @@ class Leohowl
 
   public static function formatCategory($format_category, $root_uuid = '0')
   {
-    //提取有效内容
     $category = [];
 
-    //上个循环新增的数量
     $first_loop = true;
     $new_num = 1;
     while($new_num !== 0){
       $new_num = 0;
       if($first_loop){
-        //处理首次循环
         foreach ($format_category as $key1 => $single_remain_category){
           if($single_remain_category['father'] === $root_uuid){
             $category[] = $single_remain_category;
@@ -78,26 +73,17 @@ class Leohowl
     /**
      * 将分类按级别整理成一个数组
      * 同一层级的在一个数组中
-     * 每处理完一条数据之后，将其删除
      */
-    //记录转存表
     $record_dump = [];
-    //层级记录表，二维数组,注意数组初始化,表示根层级
-    //注意数据库中取出的父级值数据类型为字符串，0初始化的时候特别注意应是字符串类型
     $level_recorder = [[$root_uuid]];
 
     $i = 0;
-    //按层级进行整理
     while(count($cate) > 0){
       foreach ($cate as $key => $value){
-        //如果一条记录的父级，在当前进行整理的层级记录表中可以找到，则可以确定这条记录属于那一级
         if(isset($level_recorder[$i])){
           if(in_array($value['father'], $level_recorder[$i])){
-            //将记录写入记录转存表
             $record_dump[$i][$value['uuid']] = $value;
-            //更新层级记录表，记录当前的层级
             $level_recorder[$i+1][] = $value['uuid'];
-            //删除当前的数据
             unset($cate[$key]);
           }
         }
@@ -105,32 +91,23 @@ class Leohowl
       $i ++;
     }
 
-    //将数组书顺序翻转,低层次的记录排在前面
     $record_dump = array_reverse($record_dump);
-    //排序后的数组
     $sorted_record = [];
-    //子节点记录
     $child_record = [];
 
-    //以层级为基础进行处理
     foreach ($record_dump as $key => $value){
-      //如果当前的子节点非空，则将改子节点赋值给相应的父节点
       if(!empty($child_record)){
         foreach ($child_record as $key1 => $value1){
           $value[$key1]['children'] = $value1;
         }
-        //重置子节点
         $child_record = [];
       }
 
-      //层级遍历
       foreach ($value as $key1 => $value1){
-        //如果当前层级的子记录还包含有父级,则将当前记录赋值给父级的子节点
         if($value1['father'] !== $root_uuid){
           $child_record[$value1['father']][] = $value1;
         }
       }
-      //将当前
       $sorted_record = $value;
 
     }
@@ -140,7 +117,7 @@ class Leohowl
 
   public static function categoryBeauty($arr,$level=0){
     $level++;
-    static $data;//定义一个静态变量用来存储最终结果
+    static $data;
     if(!$arr){
       return $arr;
     }
@@ -151,7 +128,7 @@ class Leohowl
       if (isset($val['children'])) {
         unset($temp['children']);
         $data[] = $temp;
-        self::categoryBeauty($val['children'],$level);//递归调用
+        self::categoryBeauty($val['children'],$level);
       }
       else{
         $data[] = $temp;
